2dbda7b63d57d5fb6d0d2d81a1908150dae43402
User <mm@12>
2024-07-22 19:48:52 +0300

    Initial commit. Second project.

---
+++ lib/home/HomeScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+static const String routName = 'home_screen';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
